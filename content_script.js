41cd23994c43d70f8556042fe2160d6946d9dacd
SkyAo <csvwolf@qq.com>
2016-11-04 13:40:20 +0800

    change to MutationObserver

--- content_script.js
+++ content_script.js
@@ -1,12 +1,22 @@
-let target = null;
-document.querySelector('body').addEventListener('DOMNodeInserted', () => {
-  clearTimeout(target);
-  if (document.querySelector('.table-responsive')) {
-    target = setTimeout(() => {
-      const trs = document.querySelectorAll('.reactable-data tr');
-      for (let i = 0; i < trs.length; i++) {
-        if (trs[i].querySelector('.fa-lock')) trs[i].style.display = 'none';
-      }
-    }, 100);
-  }
-});
+(function() {
+  let target = null;
+
+  const observer = new MutationObserver((mutations) => {
+    clearTimeout(target);
+    if (document.querySelector('.table-responsive')) {
+      target = setTimeout(() => {
+        const trs = document.querySelectorAll('.reactable-data tr');
+        for (let i = 0; i < trs.length; i++) {
+          if (trs[i].querySelector('.fa-lock')) trs[i].style.display = 'none';
+        }
+      }, 100);
+    }
+  });
+
+  const config = {
+    childList: true,
+    subtree: true
+  };
+
+  observer.observe(document.querySelector('body'), config);
+})();
